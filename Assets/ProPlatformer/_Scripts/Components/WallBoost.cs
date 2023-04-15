f3bf27d7c65f24db42a36c5b4ed6f7ecff9266db
macmini <liupu@outlook.com>
2023-04-16 00:25:28 +0800

    抓墙跳跃反弹

--- Assets/ProPlatformer/_Scripts/Components/WallBoost.cs
+++ Assets/ProPlatformer/_Scripts/Components/WallBoost.cs
@@ -9,10 +9,16 @@ namespace Myd.Platform
     /// </summary>
     public class WallBoost
     {
+        //墙上跳跃推动时间
         private float timer;
+        //统计帧数
+        private int boostCount;
+        //跳跃方向
         private int dir;
+
         private PlayerController controller;
         public float Timer => timer;
+
         public WallBoost(PlayerController playerController)
         {
             this.controller = playerController;
@@ -22,30 +28,46 @@ namespace Myd.Platform
 
         public void ResetTime()
         {
-            this.timer = 0;
+            this.timer = 0f;
+            this.boostCount = 0;
         }
 
+        /// <summary>
+        /// 抓墙跳跃的检测
+        /// </summary>
+        /// <param name="deltaTime"></param>
         public void Update(float deltaTime)
         {
             if (timer > 0)
             {
                 timer -= deltaTime;
+                boostCount++;
+
+                // 按键方向一致,执行反弹推动作用
                 if (controller.MoveX == dir)
                 {
                     controller.Speed.x = Constants.WallJumpHSpeed * controller.MoveX;
                     timer = 0;
+                    Myd.Common.Logging.Log("抓墙跳跃(反弹)推动速度:" + controller.Speed.x
+                        + ", 按键反应帧数:" + boostCount + ", 时长剩余: " + timer);
                 }
             }
         }
 
-        //跳跃时，激活
+        /// <summary>
+        /// 跳跃时，激活
+        /// </summary>
         public void Active()
         {
+            // x轴静止的情况下
             if (controller.MoveX == 0)
             {
-                Debug.Log("====WallBoost");
+                Debug.Log("====WallBoost抓墙跳跃(反弹)");
+                // 设置反方向(向后跳)
                 this.dir = -(int)controller.Facing;
+                // 设置按键反应时长
                 this.timer = Constants.ClimbJumpBoostTime;
+                this.boostCount = 0;
             }
         }
     }
